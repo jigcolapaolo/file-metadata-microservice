599c6eb1d5aa53c4e04a0d8644940b986ae5d0bc
Juan <Jinbyos@Gmail.com>
2025-03-03 19:29:01 -0300

    feat: added file metadata microservice

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
 
 var app = express();
 
@@ -11,10 +12,23 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const upload = multer({ storage: multer.memoryStorage() })
 
+app.post('/api/fileanalyse/', upload.single('upfile'), (req, res) => {
+
+  if (!req.file) return res.status(400).json({ error: "no file uploaded" })
+
+  const file = req.file
+
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port + `http://localhost:${port}`);
 });
